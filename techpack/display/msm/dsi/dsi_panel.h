e932023c9a4be0b64dde13196f70a90c158484b8
Erfan Abdi <erfangplus@gmail.com>
2021-01-17 13:03:02 +0330

    techpack: display: Add HBM sysfs node
    Change-Id: I70ff363d68eed71869a22c45e7849f6b1cd7c856

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -403,6 +403,8 @@ struct dsi_panel {
 	int paramNum;
 	int dc_state;
 	struct msm_param_info curDCModeParaInfo;
+
+	bool hbm_state;
 };
 
 bool dsi_display_all_displays_dead(void);
@@ -549,6 +551,7 @@ int dsi_panel_set_param(struct dsi_panel *panel,
 			struct msm_param_info *param_info);
 
 void dsi_panel_reset_param(struct dsi_panel *panel);
+void dsi_panel_set_custom_param(struct dsi_panel *panel);
 
 int dsi_panel_get_elvss_data(struct dsi_panel *panel);
 int dsi_panel_get_elvss_data_1(struct dsi_panel *panel);

eqs-development/android_kernel_motorola_sm8475

techpack: display: skip setting backlight when FOD HBM is enabled
Change-Id: I25e6b852bc7f4fd207bdae3bd295549ef390070c

techpack/display/msm/dsi/dsi_panel.c
@@ -631,6 +631,9 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 	if (panel->host_config.ext_bridge_mode)
 		return 0;
 
+	if (panel->fod_hbm_enabled || panel->hbm_enabled)
+		goto skip_set;
+
 	DSI_DEBUG("backlight type:%d lvl:%d\n", bl->type, bl_lvl);
 	switch (bl->type) {
 	case DSI_BACKLIGHT_WLED:
@@ -649,6 +652,7 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		rc = -ENOTSUPP;
 	}
 
+skip_set:
 	bl->real_bl_level = bl_lvl;
 
 	return rc;

raghavt20/kernel_sm8350

techpack/display/msm/dsi/dsi_panel.c
@@ -866,6 +866,9 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 	if (panel->host_config.ext_bridge_mode)
 		return 0;
 
+	if (panel->fod_hbm_enabled || panel->hbm_state)
+		goto skip_set;
+
 	if(panel->lhbm_config.enable) {
 		panel->lhbm_config.dbv_level = bl_lvl;
 		DSI_INFO("backlight type:%d dbv lvl:%d\n", bl->type, bl_lvl);
@@ -901,6 +904,7 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		rc = -ENOTSUPP;
 	}
 
+skip_set:
 	bl->real_bl_level = bl_lvl;
 
 	panel->fod_dim_alpha = dsi_panel_calc_fod_dim_alpha(panel, bl_lvl);
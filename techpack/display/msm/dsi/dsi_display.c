MotorolaMobilityLLC/kernel-msm-techpack-display

disp: msm: fix checking returned value of xx_cmd_transfer
Change the checking returned value of dsi_ctrl_cmd_transfer(), because
if this is a read command then it will return the number byte of
reading when reading succeed. If this is a write command, then it
will return "none-zero" for failure.

Change-Id: I713c704e7ea37d6938bac6cbce302ccc7aed5997
Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
Reviewed-on: https://gerrit.mot.com/1412939
SLTApproved: Slta Waiver
SME-Granted: SME Approvals Granted
Tested-by: Jira Key
Submit-Approved: Jira Key

msm/dsi/dsi_display.c
@@ -3159,7 +3159,8 @@ static ssize_t dsi_host_transfer(struct mipi_dsi_host *host,
 			flags |= DSI_CTRL_CMD_READ;
 		rc = dsi_ctrl_cmd_transfer(display->ctrl[ctrl_idx].ctrl, msg,
 				cmd_flags);
-		if (rc) {
+		if (((flags & DSI_CTRL_CMD_READ) && rc <= 0) ||
+				(!(flags & DSI_CTRL_CMD_READ) && rc)) {
 			DSI_ERR("[%s] cmd transfer failed, rc=%d\n",
 			       display->name, rc);
 			goto error_disable_cmd_engine;

raghavt20/kernel_sm8350

Change-Id: I088af5da728dc758470377253b8d4e16d1222c1b
Reviewed-on: https://gerrit.mot.com/1697593
Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
(cherry picked from commit 24ba2b4d63171f31d860c4de04495fcbd4bde4d8)

techpack/display/msm/dsi/dsi_display.c
@@ -3678,7 +3678,9 @@ static ssize_t dsi_host_transfer(struct mipi_dsi_host *host,
 			cmd_flags |= DSI_CTRL_CMD_READ;
 		rc = dsi_ctrl_cmd_transfer(display->ctrl[ctrl_idx].ctrl, msg,
 				&cmd_flags);
-		if (rc < 0) {
+		if (((cmd_flags & DSI_CTRL_CMD_READ) && rc <= 0) ||
+				(!(cmd_flags & DSI_CTRL_CMD_READ) && rc < 0)) {
+
 			DSI_ERR("[%s] cmd transfer failed, rc=%d\n",
 			       display->name, rc);
 			goto error_disable_cmd_engine;